2bc465de4afc2c04b8d87bfc6905875b688c37cb
Alonso <alonso.jimenez@hotmail.com>
2021-04-28 22:09:19 -0600

    Codigo inicial para pruebas de calibracion

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+const unsigned int trigger=14;
+const unsigned int echo=12;
+const unsigned int serialRate=9600;
 
+void setup() {
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(serialRate);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  
 
-}
+ const unsigned long duration= pulseIn(echo, HIGH);
+ int distance= duration/29/2;
+ if(duration==0){
+   Serial.println("Warning: no pulse from sensor");
+   } 
+  else{
+      Serial.print("distance to nearest object:");
+      Serial.println(distance);
+      Serial.println(" cm");
+  }
+ delay(100);
+ }
